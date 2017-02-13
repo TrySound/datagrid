71230c844342b0e0716fe2157e15bf22be89c3a2
Bogdan Chadkin <trysound@yandex.ru>
2017-02-13 09:33:52 +0300

    Add table in list demo

--- src/ListDemo.js
+++ src/ListDemo.js
@@ -2,16 +2,44 @@ import createElement from 'inferno-create-element';
 import Component from 'inferno-component';
 import List from './List.js';
 
-const Row = ({ datum, index }) => (
-    // console.log(datum),
-    <div style={{
-        display: 'flex',
-        alignItems: 'center',
-        padding: '0 10px',
-        height: 30,
-        background: index % 2 === 0 ? 'lightgrey': ''
-    }}>{datum}</div>
+const Column = ({ column, datum, index }) => (
+    <div style={{ width: column.width }}>
+        {datum}
+    </div>
 );
+
+const Row = ({ columns, datum, index, rowHeight, component: Column }) => (
+    <div style={{ display: 'flex', alignItems: 'stretch', height: 'inherit' }}>
+        {columns.map((item, columnIndex) =>
+            <Column column={item} datum={datum[item.name]} index={columnIndex} />
+        )}
+    </div>
+);
+
+class Table extends Component {
+    constructor() {
+        super();
+        this.component = ({ datum, index }) => (
+            <Row
+                columns={this.props.columns}
+                datum={datum}
+                index={index}
+                component={this.props.component} />
+        );
+    }
+
+    render({ data, scrollTop, viewportHeight, rowHeight }) {
+        return (
+            <List
+                data={data}
+                scrollTop={scrollTop}
+                viewportHeight={viewportHeight}
+                rowHeight={rowHeight}
+                component={this.component}
+            />
+        );
+    }
+}
 
 export default class Viewport extends Component {
     constructor() {
@@ -19,7 +47,25 @@ export default class Viewport extends Component {
 
         this.state = {
             scrollTop: 0,
-            data: Array(500000).fill(0).map((item, i) => `Title ${i}`)
+            columns: [
+                {
+                    name: 'col1',
+                    width: 100
+                },
+                {
+                    name: 'col2',
+                    width: 150
+                },
+                {
+                    name: 'col3',
+                    width: 200
+                }
+            ],
+            data: Array(500000).fill(0).map((item, i) => ({
+                col1: i,
+                col2: `Title ${i}`,
+                col3: 'lorem'
+            }))
         };
 
         this.ref = element => {
@@ -37,18 +83,18 @@ export default class Viewport extends Component {
         };
     }
 
-    render({}, { data, scrollTop, viewportHeight }) {
+    render({}, { columns, data, scrollTop, viewportHeight }) {
         return (
             <div className="viewport" style={{ width: 800, height: 400, overflow: 'auto' }}
                 onScroll={this.onScroll}
                 ref={this.ref}>
-                <List
+                <Table
+                    columns={columns}
                     data={data}
                     scrollTop={scrollTop}
                     viewportHeight={viewportHeight}
                     rowHeight={30}
-                    component={Row}
-                />
+                    component={Column} />
             </div>
         );
     }
