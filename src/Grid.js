20109b8614dacb4d329929903ee0af8e2336a546
Bogdan Chadkin <trysound@yandex.ru>
2017-02-21 01:51:39 +0300

    Move compose to utils

--- src/Grid.js
+++ src/Grid.js
@@ -5,7 +5,8 @@ import ResizeGhost from './ResizeGhost.js';
 import List from './List.js';
 import DefaultRow from './DefaultRow.js';
 import DefaultHeaderColumn from './DefaultHeaderColumn.js';
-import { compose, withPropsOnChange, withPinnableColumns } from './decorators/index.js';
+import { withPropsOnChange, withPinnableColumns } from './decorators/index.js';
+import { compose } from './utils/index.js';
 import { markMoveDest, moveColumn, moveResizeGhost, resizeColumn } from './actionCreators.js';
 import { headerZindex, defaultMinWidth } from './params.js';
 
--- src/decorators/index.js
+++ src/decorators/index.js
@@ -1,4 +1,3 @@
-export { default as compose } from './compose.js';
 export { default as withMiddleState } from './withMiddleState.js';
 export { default as withProps } from './withProps.js';
 export { default as withPropsOnChange } from './withPropsOnChange.js';
