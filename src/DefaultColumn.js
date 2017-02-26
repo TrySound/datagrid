1488ae0afb3c008fa0f47745a88675f8c247aa8a
Bogdan Chadkin <trysound@yandex.ru>
2017-02-26 19:57:22 +0300

    Prefer withHandlers decorator over linkEvent

--- src/DefaultColumn.js
+++ src/DefaultColumn.js
@@ -1,5 +1,5 @@
-import { linkEvent } from 'inferno';
 import createElement from 'inferno-create-element';
+import { withHandlers } from './decorators/index.js';
 import { defaultBorder, defaultActiveBorder, defaultHeaderBackground } from './params.js';
 import { filterColumn, sortColumn } from './actionCreators.js';
 
@@ -42,15 +42,12 @@ const Arrow = ({ direction }) => (
     </div>
 );
 
-const onSortClink = (props, event) =>
-    props.callback(sortColumn(props.column.name));
-
-const onFilterInput = (props, event) =>
-    props.callback(filterColumn(props.column.name, event.target.value));
-
-export default props => (
+export default withHandlers({
+    onSortClink: props => props.callback(sortColumn(props.column.name)),
+    onFilterInput: (props, event) => props.callback(filterColumn(props.column.name, event.target.value))
+})(props =>
     <div style={getColumnStyle(props.column, props.index, props.ghost)}>
-        <div style={{ display: 'flex', alignItems: 'center', height: 30, }} onClick={linkEvent(props, onSortClink)}>
+        <div style={{ display: 'flex', alignItems: 'center', height: 30, }} onClick={props.onSortClink}>
             {props.column.displayName || props.column.name}
             {props.column.sort &&
                 <Arrow direction={props.column.sort} />
@@ -60,7 +57,7 @@ export default props => (
             <input style={getInputStyle()}
                 placeholder={props.column.placeholder}
                 value={props.column.value}
-                onInput={linkEvent(props, onFilterInput)} />
+                onInput={props.onFilterInput} />
         }
     </div>
 );
--- src/decorators/index.js
+++ src/decorators/index.js
@@ -1,4 +1,5 @@
 export { default as shouldUpdate } from './shouldUpdate.js';
+export { default as withHandlers } from './withHandlers.js';
 export { default as withMiddleState } from './withMiddleState.js';
 export { default as withPropsOnChange } from './withPropsOnChange.js';
 export { default as withScrollProps } from './withScrollProps.js';
