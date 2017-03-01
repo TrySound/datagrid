4daf0def2bca95f7fd76d6e5fc24121d513ddbd1
Bogdan Chadkin <trysound@yandex.ru>
2017-03-01 04:29:54 +0300

    Rewrite bisectColumns with findColumn

--- src/utils/bisectColumns.js
+++ src/utils/bisectColumns.js
@@ -1,25 +1,20 @@
+import findColumn from './findColumn.js';
+
 export default (columns, position) => {
     if (columns.length < 2) {
         return [-1, 0];
     }
-    let last = 0;
-    return columns.reduce((acc, { width }, index) => {
-        const left = last;
-        const relative = position - left;
-        const half = width / 2;
-        last = left + width;
-        if (index === 0 && relative < half) {
-            return [-1, index];
-        }
-        if (index === columns.length - 1 && half <= relative) {
-            return [index, -1];
-        }
-        if (half <= relative && relative <= width) {
-            return [index, index + 1];
-        }
-        if (0 <= relative && relative <= half) {
-            return [index - 1, index];
-        }
-        return acc;
-    }, []);
+    const [columnIndex, columnPosition] = findColumn(columns, position);
+    if (columnIndex === -1) {
+        return [-1, 0];
+    }
+    if (columnIndex === columns.length) {
+        return [columns.length - 1, -1];
+    }
+    const width = columns[columnIndex].width;
+    if (columnPosition < width / 2) {
+        return [columnIndex - 1, columnIndex];
+    } else {
+        return [columnIndex, columnIndex + 1];
+    }
 };
