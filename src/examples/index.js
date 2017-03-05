fb9c6d51a030d4f95e16a451594667ac465ffe3d
Bogdan Chadkin <trysound@yandex.ru>
2017-03-05 17:43:04 +0300

    Remove List from  public api

--- src/examples/index.js
+++ src/examples/index.js
@@ -1,18 +1,9 @@
 import Inferno from 'inferno';
 import createElement from 'inferno-create-element';
-import ListDemo from './ListDemo.js';
 import GridDemo from './GridDemo.js';
 
 const container = document.createElement('div');
 container.classList.add('app');
 document.body.appendChild(container);
 
-Inferno.render(
-    <div>
-        <GridDemo />
-        <div style={{ height: 30 }}></div>
-        <ListDemo />
-        <div style={{ height: 30 }}></div>
-    </div>,
-    container
-);
+Inferno.render(<GridDemo />, container);
