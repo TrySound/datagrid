f978371b2fa62426b14571e21028fc3f71caf425
Bogdan Chadkin <trysound@yandex.ru>
2017-03-18 19:57:41 +0300

    Improve coverage

--- src/hoc/withPinnedColumns.js
+++ src/hoc/withPinnedColumns.js
@@ -1,9 +1,9 @@
 import createElement from 'inferno-create-element';
-import { compose } from '../utils/index.js';
+import { compose, checkProps } from '../utils/index.js';
 import withPropsOnChange from './withPropsOnChange.js';
 
 export default () => BaseComponent => compose(
-    withPropsOnChange((props, nextProps) => props.columns !== nextProps.columns, props => ({
+    withPropsOnChange(checkProps('columns'), props => ({
         leftPinnedColumns: props.columns.filter(column => column.pinnedLeft),
         centerColumns: props.columns.filter(column => !column.pinnedLeft && !column.pinnedRight),
         rightPinnedColumns: props.columns.filter(column => column.pinnedRight)
