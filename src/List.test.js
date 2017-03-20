a000cd3fcfca4b2c4ecf20a2a77d0f76db912492
Bogdan Chadkin <trysound@yandex.ru>
2017-03-20 22:51:08 +0300

    Simplify list test

--- src/List.test.js
+++ src/List.test.js
@@ -3,21 +3,10 @@ import createElement from 'inferno-create-element';
 import { mount } from './utils/testUtils.js';
 import List from './List.js';
 
-const renderList = props => mount(List).setProps(props);
-const getListDivs = props => {
-    const { setProps, getWrapper } = mount(List);
-    setProps(Object.assign({
-        component: props => <div>{props.datum}</div>
-    }, props));
-    return Array.from(getWrapper().querySelectorAll('div'));
-};
-const getContainer = list => list[0];
-const getRowWrapper = (list, index) => list[2 + index * 2];
-
 describe('List component', () => {
     it('passes datum and index or row to component', () => {
         const result = [];
-        renderList({
+        mount(List).setProps({
             scrollTop: 0,
             viewportHeight: 60,
             rowHeight: 30,
@@ -30,39 +19,29 @@ describe('List component', () => {
         ]);
     });
 
-    it('19ths row edge case', () => {
-        const result = [];
-        renderList({
-            scrollTop: 336 - 60,
-            viewportHeight: 360 - 60,
-            rowHeight: 30,
-            data: Array(40).fill(0).map((_, index) => `data${index}`),
-            component: props => result.push(props)
-        });
-        expect(result).toEqual(Array(21).fill(0).map((_, index) => ({
-            datum: `data${index}`,
-            index
-        })));
-    });
-
     it('container height is a sum of all rows', () => {
-        const list = getListDivs({
+        const { setProps, getWrapper } = mount(List);
+        setProps({
             scrollTop: 0,
             viewportHeight: 100,
             rowHeight: 30,
-            data: ['data1', 'data2']
+            data: ['data1', 'data2'],
+            component: () => {}
         });
-        expect(getContainer(list).style.height).toEqual('60px');
+        expect(getWrapper().children[0].style.height).toEqual('60px');
     });
 
     it('each row wrapper should have height', () => {
-        const list = getListDivs({
+        const { setProps, getWrapper } = mount(List);
+        setProps({
             scrollTop: 0,
             viewportHeight: 100,
             rowHeight: 30,
-            data: ['data1', 'data2']
+            data: ['data1', 'data2'],
+            component: () => {}
         });
-        expect(getRowWrapper(list, 0).style.height).toEqual('30px');
-        expect(getRowWrapper(list, 1).style.height).toEqual('30px');
+        const rows = getWrapper().children[0].children[0].children;
+        expect(rows[0].style.height).toEqual('30px');
+        expect(rows[1].style.height).toEqual('30px');
     });
 });
